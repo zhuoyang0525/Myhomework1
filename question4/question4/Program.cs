5b263026bd97e7a51b715e0dbb30f869c8cfb641
晨阳 司 <2017302580203@whu.edu.cn>
2018-09-10 21:44:11 +0800

    第一次作业

---
+++ question4/question4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace question4
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string s = "";
+			double a = 0;
+			double b = 0;
+			Console.Write("Please input two numbers:");
+			s = Console.ReadLine();
+			a = Double.Parse(s);
+			s = Console.ReadLine();
+			b = Double.Parse(s);
+			Console.WriteLine("The product of these numbers is:" + (a * b));
+		}
+	}
+}
